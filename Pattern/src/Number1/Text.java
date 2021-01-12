e353f364b352a9d526b218ee862c7f8074c90dac
HP <HP@DESKTOP-MB3IDCM>
2021-01-13 00:19:58 +0530

    Pattern

---
+++ Pattern/src/Number1/Text.java
@@ -0,0 +1,15 @@
+package Number1;
+public class Text 
+{
+	public static void main(String[] ar)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.print("\n");
+		}
+	}
+}
