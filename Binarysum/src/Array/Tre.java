e8e66bcd224bd70c8ce8b363cd213dfea1b55ad9
HP <swamisonamsingh576@gmail.com>
2021-01-30 18:21:09 +0530

    method 2

--- Binarysum/src/Array/Tre.java
+++ Binarysum/src/Array/Tre.java
@@ -2,49 +2,35 @@ package Array;
 import java.util.*;
 public class Tre 
 {
-	public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+	public static void main(String[] args)
+	{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int a[] = new int[n];
-        for(int i=0;i<n;i++) {
-            a[i] = scanner.nextInt();
-        }
-        int k = scanner.nextInt();
-        int b[] = new int[k];
-        for(int i=0;i<k;i++) {
-            b[i] = scanner.nextInt();
-        }
-        int sum = scanner.nextInt();
-
-        findSum(a, b, sum);
-    }
-
-    public static void findSum(int a[], int b[], int sum)
-    {
-
         for(int i=0;i<a.length;i++) 
         {
-            int elementToFind = sum - a[i];
-            int start = 0;
-            int end = b.length - 1;
-            while (start <= end) 
-            {
-                int mid = (start + end) / 2;
-                if(elementToFind == b[mid]) 
-                {
-                	System.out.println(a[i]+" "+b[mid]);
-                    break;
-                }
-                if(elementToFind < b[mid]) 
-                {
-                    end = mid - 1;
-                } 
-                else 
-                {
-                    start = mid + 1;
-                }
-            }
+            a[i] = sc.nextInt();
+        }
+        int element=sc.nextInt();
+        int start=0;
+        int end=a.length-1;
+        //int index=-1;
+        while(start<end)
+        {
+    	 int mid=(start+end)/2;
+    	 if(a[mid]==element)
+    	 {
+    		 System.out.print(mid);
+    		 break;
+    	 }
+    	 else if(a[mid]>element)
+    	 {
+    		 end=mid-1;
+    	 }
+    	 else
+    	 {
+    		 start=mid+1;
+    	 }
         }
     }
 }
